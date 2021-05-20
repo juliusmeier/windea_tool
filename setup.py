9cf5e684e748ca4453fab4ae8de1448d163003f2
unknown <julius.meier@gmx.de>
2021-05-20 18:03:03 +0200

    Add package_data in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     license='MIT',
     description='An example of a python package from pre-existing code',
     long_description=open('README.md').read(),
+    url="https://github.com/juliusmeier/windea_tool",
+
+    include_package_data=True,          # https://kiwidamien.github.io/making-a-python-package-vi-including-data-files.html
+    package_data={'': ['data/*.xlsx']}, # https://godatadriven.com/blog/a-practical-guide-to-using-setup-py/
 
     # Needed to actually package something
     packages=['windea_tool'],  # directory containing __init__.py
